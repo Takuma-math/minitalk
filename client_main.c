558aeb03c0027d9a4676759e65d035846e88d933
Takuma Hayashi <takhayas@c2r4s4.42tokyo.jp>
2025-10-28 14:11:06 +0900

    1028 commit

--- client_main.c
+++ client_main.c
@@ -6,21 +6,11 @@
 /*   By: takhayas <hayatakucat@icloud.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:09:12 by takhayas          #+#    #+#             */
-/*   Updated: 2025/10/27 15:43:26 by takhayas         ###   ########.fr       */
+/*   Updated: 2025/10/27 20:46:38 by takhayas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-void	signal_handler(int signo)
-{
-	static int	bit_counter;
-	static char	current_char;
-
-	if (signo == SIGUSR2)
-		current_char = current_char | 1 << bit_counter;
-	bit_counter++;
-}
 
 int	main(int argc, char **argv)
 {
--- server_main.c
+++ server_main.c
@@ -6,16 +6,45 @@
 /*   By: takhayas <hayatakucat@icloud.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 14:37:48 by takhayas          #+#    #+#             */
-/*   Updated: 2025/10/27 15:32:24 by takhayas         ###   ########.fr       */
+/*   Updated: 2025/10/27 21:23:44 by takhayas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int signo)
+{
+	static int	bit_counter;
+	static char	current_char;
+
+	if (signo == SIGUSR2)
+		current_char = current_char | (1 << bit_counter);
+	bit_counter++;
+	if (bit_counter == 8)
+	{
+		if (current_char == '\0')
+			write (1, "\n", 1);
+		else
+			write (1, &current_char, 1);
+	}
+	bit_counter = 0;
+	current_char = 0;
+	return ;
+}
+
+void	set_sigaction(struct sigaction *sa)
+{
+	sa->sa_handler = signal_handler;
+	sigemptyset(&sa->sa_mask);
+	sigaddset(&sa->sa_mask, SIGUSR1);
+	sigaddset(&sa->sa_mask, SIGUSR2);
+	sa->sa_flags = 0;
+}
+
 int	main(void)
 {
-	pid_t	server_pid;
+	struct sigaction	sa;
 
-	server_pid = getpid();
-	ft_printf("%d\n", server_pid);
+	ft_printf("%d\n", getpid());
+	set_sigaction(&sa);
 }
